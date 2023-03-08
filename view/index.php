30d9c443784d6a072327503a16a70aebea8e3775
Vincent Woglo <vincentwoglo@gmail.com>
2023-03-07 21:00:34 -0700

    Homepage header

--- view/index.php
+++ view/index.php
@@ -3,7 +3,10 @@
     $twig = new \Twig\Environment($loader);
 
     echo $twig->render('index.html.twig', [
-        'stylesheet' => '/Munchy/view/style/css/index.css', //could be nested array also
+        'stylesheet' => [
+            '/Munchy/view/style/css/index.css',
+            '/Munchy/view/style/css/style.css'
+        ], //could be nested array also
         'javascriptModule' => [
             '/Munchy/view/script/login/ajaxLogin.js',
             '/Munchy/view/script/login/validation.js',
