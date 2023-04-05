a6ddb7dd268607cb46d8391c6fae053dd235dd27
Vincent Woglo <vincentwoglo@gmail.com>
2023-04-04 22:05:47 -0600

    dashboard menu

--- view/dashboard.php
+++ view/dashboard.php
@@ -2,5 +2,13 @@
     $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates/dashboard');
     $twig = new \Twig\Environment($loader);
 
-    echo $twig->render('home.html', []);
+    echo $twig->render('home.html', [
+        'stylesheet' => '/Munchy/view/style/css/dashboard.css', //could be nested array also
+        'nav_logo' => '/Munchy/view/assets/image/logo_icon.png',
+        'javascriptModule' => [
+            '/Munchy/view/script/login/ajaxLogin.js',
+            '/Munchy/view/script/login/validation.js',
+        ],
+        'javascript' => []
+    ]);
 ?>
