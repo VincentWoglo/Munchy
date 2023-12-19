7effdf1294ae6ccb64818d57e79ead3295f38749
VincentWoglo <vincentwoglo@gmail.com>
2023-12-19 14:17:10 -0700

    Dashboard register, login and image icons

--- view/dashboard.php
+++ view/dashboard.php
@@ -3,8 +3,11 @@
     $twig = new \Twig\Environment($loader);
 
     echo $twig->render('home.html', [
-        'stylesheet' => '/Munchy/view/style/css/dashboard.css', //could be nested array also
+        'stylesheet' => '/Munchy/view/style/css/dashboard.css', //could be array also
         'nav_logo' => '/Munchy/view/assets/image/logo_icon.png',
+        'nav_todo_list' => '/Munchy/view/assets/image/check-square.svg',
+        'notification_bell_icon' => '/Munchy/view/assets/image/bell.svg',
+        'nav_search_icon' => '/Munchy/view/assets/image/search.svg',
         'javascriptModule' => [
             '/Munchy/view/script/login/ajaxLogin.js',
             '/Munchy/view/script/login/validation.js',
