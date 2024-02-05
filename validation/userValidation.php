4a2b13e70b43608a538be44b7471f2f2dc8eb6cc
VincentWoglo <vincentwoglo@gmail.com>
2024-02-05 02:45:48 -0700

    user validation server-side

--- validation/userValidation.php
+++ validation/userValidation.php
@@ -1,61 +1,45 @@
 <?php
     namespace munchy\validation;
 
-    abstract class validation {
-        public function __construct() {
-            
-        }
-
+    class userValidation extends validation {
 
         /**
-         * check if the string of the user is at least the minimum character
-         * Returns a false if the length is not min length
+         * Checks if password is correct length and contains min special characters
+         * Returns message in array including error
          * 
-         * @return bool
+         * @return array
+         * 
          */
-        public function isMinLength($userPassword, $minChars): bool
+        protected function isValidPassword($userPassword, $minPasswordLength) : array
         {
-            return count($userPassword) >= $minChars;
+            $returnMessage = [];
+
+            // Define validation checks and their corresponding methods
+            $checks = [
+                'ContainsInteger' => 'containsInteger',
+                'ContainsSpecialChars' => 'containsSpecialChars',
+                'IsMinLength' => 'isMinLength',
+            ];
+        
+            // Check each condition and push messages to $returnMessage
+            foreach ($checks as $messageKey => $methodName) {
+                if($this->$methodName($userPassword))
+                    $returnMessage[] = [$messageKey => true];
+            }
+
+            return $returnMessage;
         }
 
 
         /**
-         * Check if the email is valid with right
-         * Check if if email is not example at example.com
+         * @return array
          * 
-         * @return bool
          */
-        public function containsAtSymbol($userInput) : bool
+        protected function isValidEmail()
         {
-            return str_contains($userInput, '@');
+
         }
 
-
-        /**
-         * Check if the user is using a valid email client
-         * Checks for popular email clients
-         * 
-         * @return bool
-         */
-        public function isValidEmailClient(){}
-
-
-        /**
-         * Check if the input contains numbers [0-9]
-         * 
-         * @return bool
-         */
-        public function containsInteger($userInput) : bool
-        {
-            return preg_match('/\d/', $userInput) === 1;
-        }
-
-
-        /**
-         * Check if the input contains special characters. Ex: ~!@#$%^&*()
-         * 
-         * @return bool
-         */
-        public function containsSpecialChars(){}
+        
     }
 ?>
